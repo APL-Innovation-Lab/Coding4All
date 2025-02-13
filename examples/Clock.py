3a653b57e54108119c3012c672c7c187168b2d15
Martha Cryan <martha.cryan326@gmail.com>
2025-02-13 17:44:25 -0600

    Update clock sample code

--- examples/Clock.py
+++ examples/Clock.py
@@ -1,48 +1,33 @@
-# Import all the functions from tkinter library
+# importing whole module
 from tkinter import *
+from tkinter.ttk import *
 
-# Specifically import ttk from tkinter library
-from tkinter import ttk
+# importing strftime function to
+# retrieve system's time
+from time import strftime
 
-# Import datetime class from datetime module
-from datetime import datetime
-
-# Get the current date and time
-now = datetime.now()
-
-# Format the current date and time
-current_time = now.strftime("%I:%M:%S")
-
-# Print the current date and time
-print("Current Time =", current_time)
-
-# Instantiate the "Tk" class
-# This means we want to use tkinter to make a window
+# creating tkinter window
 root = Tk()
-width=1000
-height=500
+root.title('Clock')
 
-# Make a "frame"
-canvas = Canvas(root, width=width, height=height)
-label = canvas.create_text( width/2, height/2, text=current_time, font=("Arial", 150))
+# This function is used to
+# display time on the label
 
-# Create a function to update the time
-def update_time():
-    # get the current time
-    now = datetime.now()
+def time():
+    string = strftime('%H:%M:%S %p')
+    lbl.config(text=string)
+    lbl.after(1000, time)
 
-    # format the current time
-    current_time = now.strftime("%I:%M:%S")
 
-    # update the label with the current time
-    canvas.itemconfig(label,text=current_time)
-    canvas.pack(anchor=CENTER)
+# Styling the label widget so that clock
+# will look more attractive
+lbl = Label(root, font=('calibri', 40, 'bold'),
+            background='purple',
+            foreground='white')
 
-    # run itself again after 1000 ms
-    root.after(1000, update_time)
+# Placing clock at the centre
+# of the tkinter window
+lbl.pack(anchor='center')
+time()
 
-# run the function update_time for the first time
-update_time()
-
-# Run the window
-root.mainloop()
+mainloop()
